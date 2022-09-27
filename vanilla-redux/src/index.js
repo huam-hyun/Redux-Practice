8acb9e7b3c278d197ae797a60b3c9ba4529f7953
huam-hyun <duddjshfl@naver.com>
2022-09-27 17:05:02 +0900

    simple redux

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,23 +1,25 @@
+import { createStore } from 'redux';
+
 const plus = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-let count = 0;
-number.innerText = count;
-
-const updateText = () =>{
-  number.innerText = count;
+// reducer 유일하게 data를 바꿀 수 있는 곳
+const countModifier = (count = 0, action) => {
+  if(action.type === 'ADD'){
+    return count + 1;
+  } else if(action.type === 'MINUS'){
+    return count - 1;
+  } else{
+    return count;
+  }
 }
 
-const handleAdd = () =>{
-  count += 1;
-  updateText();
-}
+// createStore와 함께 reducer 함수를 주면 dispatch시 자동으로 reducer함수로 연결해줌
+const countStore = createStore(countModifier); 
 
-const handleMinus = () =>{
-  count -= 1;
-  updateText();
-}
+// dispatch를 통해 reducer로 action을 줄 수 있다.(반드시 객체 형태로 주어야 함)
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'MINUS' });
 
-plus.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+console.log(countStore.getState());
