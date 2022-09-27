85b3b7c50db71bf157b2cef8c77c28976726f59b
SangHyun <duddjshfl@naver.com>
2022-09-28 02:13:50 +0900

    mapStateToProps 예제 추가

--- react_redux_example/src/routes/Home.js
+++ react_redux_example/src/routes/Home.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
+import { connect } from 'react-redux';
 
-function Home() {
+function Home({ toDos }) {
     const [text, setText] = useState("");
-
+    
     function onChange(e) {
         setText(e.target.value);
     }
@@ -19,8 +20,18 @@ function Home() {
                 <input type="text" value={text} onChange={onChange} />
                 <button>Add</button>
             </form>
+            <ul>
+                {JSON.stringify(toDos)}
+            </ul>
         </>
     )
 }
 
-export default Home;
+
+// redux store로 부터 가져온 state는 컴포넌트의 props에 들어간다
+function mapStateToProps(state, ownProps) {
+    return { toDos: state }
+}
+
+// connect를 통해 store와 component를 연결한다
+export default connect(mapStateToProps)(Home);
