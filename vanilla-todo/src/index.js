748aac71d0f711bf504b483deb0d142941dae530
SangHyun <duddjshfl@naver.com>
2022-09-27 23:58:45 +0900

    Redux를 이용한 vanilla ToDo

---
+++ vanilla-todo/src/index.js
@@ -0,0 +1,73 @@
+import { createStore } from 'redux';
+
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const ul = document.querySelector('ul');
+
+const ADD_TODO = 'ADD_TODO';
+const DELETE_TODO = 'DELETE_TODO';
+
+const addToDo = (text) => {
+  return {
+    type: ADD_TODO,
+    text
+  };
+};
+
+const deleteToDo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id
+  };
+};
+
+// Never Mutate State
+// Only way to mutate state is action 
+const reducer = (state = [], action) => {
+  switch(action.type){
+    case ADD_TODO:
+      return [{ text: action.text, id: Date.now() }, ...state];
+    case DELETE_TODO:
+      return [...state.filter(toDos => toDos.id !== parseInt(action.id))];
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text));
+};
+
+const dispatchDeleteToDo = (e) =>{
+  const id = e.target.parentNode.id;
+  store.dispatch(deleteToDo(id));
+}
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = '';
+  toDos.forEach(toDo => {
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    btn.innerText = 'DEL';
+    btn.addEventListener('click', dispatchDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  })
+}
+
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  dispatchAddToDo(toDo);
+};
+
+store.subscribe(() => console.log(store.getState()));
+store.subscribe(paintToDos);
+
+form.addEventListener('submit', onSubmit);
