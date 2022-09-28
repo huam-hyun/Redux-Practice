0e109349b84b808e09b06d99467b977eed9adc21
huam-hyun <duddjshfl@naver.com>
2022-09-28 17:21:00 +0900

    react-redux로 ToDo리스트 만들기

--- react_redux_example/src/routes/Home.js
+++ react_redux_example/src/routes/Home.js
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
 import { connect } from 'react-redux';
+import { actionCreators } from '../store'
+import ToDo from "../components/ToDo";
 
-function Home({ toDos }) {
+function Home({ toDos, addToDo }) {
     const [text, setText] = useState("");
-    
+
     function onChange(e) {
         setText(e.target.value);
     }
 
     function onSubmit(e) {
         e.preventDefault();
-        console.log(text);
+        addToDo(text);
+        setText('');
     }
 
     return (
@@ -21,17 +24,24 @@ function Home({ toDos }) {
                 <button>Add</button>
             </form>
             <ul>
-                {JSON.stringify(toDos)}
+                {toDos.map(toDo => <ToDo {...toDo} key={toDo.id} />)}
             </ul>
         </>
     )
 }
 
 
-// redux store로 부터 가져온 state는 컴포넌트의 props에 들어간다
+// redux store로부터 가져온 state는 컴포넌트의 props에 들어간다
 function mapStateToProps(state, ownProps) {
     return { toDos: state }
 }
 
+// redux store의 dispatch를 props에 넣어주는 함수
+function mapDispatchToProps(dispatch){
+    return { 
+        addToDo: (text) => dispatch(actionCreators.addToDo(text))
+    }
+}
+
 // connect를 통해 store와 component를 연결한다
-export default connect(mapStateToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
